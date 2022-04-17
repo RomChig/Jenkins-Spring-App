a50f8dc6927a4720682c030e2627ef75a077836e
Roman Ihnatov <roman.ihnatov@ibm.com>
2022-04-17 15:25:12 +0300

    refactoring

--- jenkins/createJob.groovy
+++ jenkins/createJob.groovy
@@ -1,8 +1,50 @@
-pipelineJob('pipelineJob') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
+//pipelineJob('pipelineJob') {
+//    definition {
+//        cps {
+//            script(readFileFromWorkspace('pipelineJob.groovy'))
+//            sandbox()
+//        }
+//    }
+//}
+
+pipeline {
+    agent any
+    stages {
+
+//        stage('go to the seed job workspace') {
+//            steps {
+//                sh "cd ../../seed-job/workspace/"
+//            }
+//        }
+        stage('check maven & java version') {
+            steps {
+                sh "mvn -v"
+                sh "java --version"
+            }
+        }
+
+        stage('unit tests') {
+            steps {
+                sh 'mvn test'
+            }
+        }
+
+        stage('package') {
+            steps {
+                sh 'mvn package'
+            }
+        }
+
+        stage('report') {
+            steps {
+                step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+            }
+        }
+
+        stage('artifact') {
+            steps {
+                step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
+            }
         }
     }
 }
