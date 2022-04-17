e8ab328e1d20b52823d248d9633f7f612205f0e4
Roman Ihnatov <roman.ihnatov@ibm.com>
2022-04-17 14:23:59 +0300

    refactoring

--- jenkins/createJob.groovy
+++ jenkins/createJob.groovy
@@ -1,7 +1,7 @@
 pipelineJob('pipelineJob') {
     definition {
         cps {
-            script(readFileFromWorkspace('./jenkins/pipelineJob.groovy'))
+            script(readFileFromWorkspace('pipelineJob.groovy'))
             sandbox()
         }
     }
