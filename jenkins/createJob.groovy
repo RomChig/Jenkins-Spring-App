71c333871b0e44c177f427387db9ab8b0b28b8eb
Roman Ihnatov <roman.ihnatov@ibm.com>
2022-04-17 14:47:02 +0300

    refactoring

--- jenkins/createJob.groovy
+++ jenkins/createJob.groovy
@@ -1,9 +1,7 @@
 pipelineJob('pipelineJob') {
     definition {
-        cpsScm {
-            scm {
-                github('RomChig/Jenkins-Spring-App', 'main')
-            }
+        scm {
+            github('RomChig/Jenkins-Spring-App', 'main')
         }
         cps {
             script(readFileFromWorkspace('pipelineJob.groovy'))
