9c2a30bad36f4c43cca322507e278846ae3a355e
Roman Ihnatov <roman.ihnatov@ibm.com>
2022-04-17 13:49:28 +0300

    refactoring

--- jenkins/pipelineJob.groovy
+++ jenkins/pipelineJob.groovy
@@ -1,14 +1,9 @@
 pipeline {
-    agent {
-        docker {
-            image 'maven:3.8.1-adoptopenjdk-11'
-            args '-v $HOME/.m2:/root/.m2'
-        }
-    }
+    agent any
     stages {
         stage('check maven & java version') {
             steps {
-                sh "./mvnw -v"
+                sh "mvn -v"
                 sh "java -version"
             }
         }
